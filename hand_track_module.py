7ede3cf08133c499d5153df9eaa0e33c4075a5fc
Eric Guan <113262615+ericguan04@users.noreply.github.com>
2023-10-19 23:36:09 -0400

    Work on hand tracking module

--- hand_track_module.py
+++ hand_track_module.py
@@ -13,18 +13,22 @@ class HandDetection():
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
-imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-results = hands.process(imgRGB)
+        def find_hands(self, img, draw = True):
+            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results = self.hands.process(imgRGB)
 
-if results.multi_hand_landmarks:
-    for handLms in results.multi_hand_landmarks:
-        for id, lm in enumerate(handLms.landmark):
-            h, w ,c = img.shape
-            cx, cy = int(lm.x * w), int(lm.y * h)
-            if id == 0:
-                cv2.circle(img, (cx,cy), 20, (255,0,255), cv2.FILLED)
-        mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+            if results.multi_hand_landmarks:
+                for handLms in results.multi_hand_landmarks:
+                    if draw:
+                        self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
 
+'''
+for id, lm in enumerate(handLms.landmark):
+    h, w ,c = img.shape
+    cx, cy = int(lm.x * w), int(lm.y * h)
+    if id == 0:
+        cv2.circle(img, (cx,cy), 20, (255,0,255), cv2.FILLED)
+'''
 
 def main():
     pTime = 0
