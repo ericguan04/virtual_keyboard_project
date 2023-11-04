a5d2c92d999dccf02ed73fab809c046b6948922c
Eric Guan <113262615+ericguan04@users.noreply.github.com>
2023-11-03 23:23:18 -0400

    Start database

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200, nullable = False))
+    content = db.Column(db.String(200), nullable = False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
